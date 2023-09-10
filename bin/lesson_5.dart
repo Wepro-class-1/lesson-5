2532f782e4258c88070c655b3ac8ba0ce6ff8551
Samir-Sattarov <samirsattarov2004@gmail.com>
2023-09-10 13:57:54 +0500

    lesson-5

---
+++ bin/lesson_5.dart
@@ -0,0 +1,46 @@
+void main() {
+  // Function - Функции
+  final a = 4;
+  final b = 6;
+  // Функция
+  // sum();
+
+  // Функция с параметрами
+  // sum(a, b);
+
+  // Функция с имменованными параметрами
+  // sum(firstParameter: a, secondParameter: b);
+}
+
+// Функция
+//sum() {}
+
+// Функция с параметрами
+// sum(int a, int b) {
+// final result = a + b;
+//
+// print("Result is $result");
+// }
+
+// Функция с имменованными параметрами
+// sum({required int firstParameter, required int secondParameter}) {
+//   final result = firstParameter + secondParameter;
+
+//   print("Result is $result");
+// }
+
+// Функция которая возвращает значение.
+
+// sum(int a, int b) {
+//   final result = a + b;
+
+//   return result;
+// }
+
+// Вариант 2 указания возвращаемого типа.
+
+// int sum(int a, int b) {
+//   final result = a + b;
+
+//   return result;
+// }
